e56530765f272ea3a15b421287a96c9ae0ec9f42
After-Aero <diabolicdevildog@gmail.com>
2026-08-12 20:52:05 +0200

    tidal prompt

--- src/data/campaign/DASubject71BattleCreationPlugin.java
+++ src/data/campaign/DASubject71BattleCreationPlugin.java
@@ -1,30 +1,15 @@
 package data.campaign;
 
-import com.fs.starfarer.api.combat.BattleCreationContext;
 import com.fs.starfarer.api.combat.CombatEngineAPI;
 import com.fs.starfarer.api.impl.combat.BattleCreationPluginImpl;
-import com.fs.starfarer.api.mission.MissionDefinitionAPI;
 
 /**
  * Combat setup used only by the normal-mode Last Line simulator battle.
  */
 public class DASubject71BattleCreationPlugin extends BattleCreationPluginImpl {
-    private static final String SIMULACRUM_BACKGROUND =
-            "graphics/da/backgrounds/diableavionics_simulacrum.png";
-
-    @Override
-    public void initBattle(
-            BattleCreationContext context,
-            MissionDefinitionAPI loader
-    ) {
-        super.initBattle(context, loader);
-        loader.setBackgroundSpriteName(SIMULACRUM_BACKGROUND);
-    }
-
     @Override
     public void afterDefinitionLoad(CombatEngineAPI engine) {
         super.afterDefinitionLoad(engine);
-        engine.setRenderStarfield(true);
 
         DASimulacrumBackgroundRenderer background =
                 new DASimulacrumBackgroundRenderer(true);
